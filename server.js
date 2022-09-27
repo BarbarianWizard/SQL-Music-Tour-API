296fffd03a9b0c4365417ca0c53db0d6a44090db
Kevin <aubeck79@gmail.com>
2022-09-26 19:52:56 -0500

    cleaned up comments

--- server.js
+++ server.js
@@ -29,10 +29,3 @@ app.listen(process.env.PORT, () => {
     console.log(`🎸 Rockin' on port: ${process.env.PORT}`)
 })
 
-/*
-sequelize model:generate --name Event --attributes "event_id:integer, name:string, date:date, start_time:date, end_time:date"
-sequelize model:generate --name Stage --attributes "stage_id:integer, stage_name:string"
-sequelize model:generate --name Stage_Events --attributes "stage_events_id:integer, stage_id:smallint, event_id:smallint"
-sequelize model:generate --name Set_Time --attributes "event_id:smallint, stage_id:smallint, band_id:smallint, start_time:date, end_time:date, set_time_id:integer"
-sequelize model:generate --name Meet_Greet --attributes "event_id:smallint, band_id:smallint, meet_start_time:date, meet_end_time:date, meet_greet_id:integer"
-*/
